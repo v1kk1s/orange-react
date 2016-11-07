b403d4e5a5996dcbd5d8e65192a792c5e49c8b4c
vikkis <baheryan.vika@gmail.com>
2016-11-07 17:21:53 +0200

    Set up webpack + babel + react

---
+++ app/index.js
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Hello = React.createClass({
+  render: function() {
+    return(
+      <div> Hello react programm! </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <Hello />,
+  document.getElementById('app')
+);
